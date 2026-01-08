941dcfcaa53c9e0d286100c0d6b17a23e68761b2
tanmay-josh <tanmay.tambat@joshsoftware.com>
2026-01-08 14:30:25 +0530

    added non-integer input check

--- submissions/tanmay_tambat/factorial.cpp
+++ submissions/tanmay_tambat/factorial.cpp
@@ -4,13 +4,24 @@ using namespace std;
 int main(){
     int n;
     cout<<"Enter the number (0-20): ";
-    cin>>n;
-    while (n < 0 || n > 20) {
+    cin >> n;
+
+    while (true) {
+        if (cin.fail()) {
+            cin.clear();
+            cin.ignore(1000, '\n');
+            cout << "Invalid input. Please enter an integer between 0 and 20: ";
+            cin >> n;
+            continue;
+        }
         if (n < 0) {
             cout << "Number cannot be less than zero. Enter again: ";
+        } else if (n > 20) {
+            cout << "Factorial too large to compute using long long.\nEnter a number between 0 and 20: ";
         } else {
-            cout << "Factorial too large to compute using long long."<<endl<<"Enter a number between 0 and 20: ";
+            break;
         }
+
         cin >> n;
     }
 
